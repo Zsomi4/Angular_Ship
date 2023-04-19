bcbd4f90a97eeb19ff2493a5cf56521ec6aaf267
Zsomi4 <k72302872172@ktch.hu>
2023-04-19 13:58:11 +0200

    Hajók adatainak kiíráa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './shared/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ship';
+export class AppComponent implements OnInit {
+
+  ships:any = [];
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.getShips();
+  }
+
+  getShips() {  
+    this.api.getShips().subscribe({
+      next: (ships) => {
+        console.log(ships)
+        this.ships = ships;
+      },
+      error: (err) => {
+        console.log('Hiba! A REST API elérése sikertelen!')
+        console.log(err)
+      }
+    });
+  }
 }
