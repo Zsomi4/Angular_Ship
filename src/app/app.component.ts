38b76ec60c027405aca09be1d91d43ac64e9e46f
Zsomi4 <k72302872172@ktch.hu>
2023-04-19 14:55:51 +0200

    Hajó és fejlécek hozzáadása

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
+/*
+ File: app.components.ts
+ Author: Gubis Zsombor Dániel
+ Copyright: 2022, Gubis Zsombor Dániel
+ Group: Szoft_II_N
+ Date: 2023-04-19
+ Github: https://github.com/zsomi4/
+ Licenc: GNU GPL
+*/
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './shared/api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -27,4 +37,33 @@ export class AppComponent implements OnInit {
       }
     });
   }
+
+  addShips(ship: any) {
+    this.api.addShips(ship).subscribe({
+      next: (newShip) => {
+        console.log('Az új hajó hozzáadva a listához:');
+        console.log(newShip);
+        this.ships.push(newShip);
+      },
+      error: (err) => {
+        console.log('Hiba a hajó hozzáadása során:');
+        console.log(err);
+      }
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const newShip = {
+        name: form.value.name,
+        length: form.value.length,
+        price: form.value.price,
+        person: form.value.person,
+        trailer: form.value.trailer
+      };
+      this.addShips(newShip);
+      form.reset();
+    }
+  }
+  
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,19 +1,31 @@
+/*
+ File: app.service.ts
+ Author: Gubis Zsombor Dániel
+ Copyright: 2022, Gubis Zsombor Dániel
+ Group: Szoft_II_N
+ Date: 2023-04-19
+ Github: https://github.com/zsomi4/
+ Licenc: GNU GPL
+*/
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  url = "http://localhost:8000/ships";
   constructor(private http: HttpClient) { }
   
   getShips() {
-    let endpoint = 'ships'
-    let url = environment.api + endpoint;
-    return this.http.get<any>(url);
+    return this.http.get<any>(this.url);
   }
+
+  addShips(ship: any) {
+    let httpHeader = new HttpHeaders({'Content-Type': 'application/json'});
+    let httpOptions = {
+      headers: httpHeader
+    };
+    return this.http.post(this.url, ship, httpOptions);
+  }  
 }
-
-
